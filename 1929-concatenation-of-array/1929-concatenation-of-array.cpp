d676721dd3671e91eecd24d6646477eccf82d06d
gurnoork-k <152222637+gurnoork-k@users.noreply.github.com>
2026-08-16 23:10:26 +0530

    Time: 4 ms (3.03%), Space: 16.6 MB (99.87%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -6,8 +6,7 @@ public:
         vector<int> ans(N);
         
         for(int i=0; i< n; i++){
-            ans[i] = nums[i];
-            ans[i+n] = nums[i];
+            ans[i] = ans[i+n] = nums[i];
         }
 
         return ans;
